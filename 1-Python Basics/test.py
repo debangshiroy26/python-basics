13af3ddb7f8557cde35199ada1c8f38cf94418ae
Debangshi Roy <debangshi.roy03@gmail.com>
2026-08-14 02:06:04 +0530

    Add initial Jupyter notebooks and Python files for basic concepts and comments

---
+++ 1-Python Basics/test.py
@@ -0,0 +1,7 @@
+## this is a single line comment. Works for both python and jupiter notebook files.
+
+'''
+# this is 
+a multi-line
+comment. And it does not work on the jupiter notebook file.
+'''
